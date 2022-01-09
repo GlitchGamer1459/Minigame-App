67ee9fb7268522532494dc6a6fe8c2697e7ac9e1
GlitchGamer1459 <glitchgamer1459@gmail.com>
2022-01-08 18:59:35 -0800

    attempting to streamline highlight code in rook class. i need a therapist now.

--- src/Application/chess/pieces/Rook.java
+++ src/Application/chess/pieces/Rook.java
@@ -35,6 +35,8 @@ public class Rook extends Piece {
                 tileRef[scanIndex].setAsViable(on);
 
                 running = false;
+            } else {
+                running = false;
             }
         }
 
@@ -56,118 +58,16 @@ public class Rook extends Piece {
 
         int posIndex = Coordinate.getIndexFromCoordinate(position);
 
-        /*
-        THIS IS DISGUSTING. NO. WHY DOES NOTHING ELSE WORK. WHAT THE F***.
-        I COULD MAAAYBE USE FOR LOOPS BUT LIKE. WHY MAKE MY LIFE EASY.
-        this is seriously bad, never do this again. 7 nested if else blocks. christ.
-         */
+        int limiter = position.x + (7 - position.x);
 
-        switch (position.x) {
-            case 0 -> {
-                if (tileRef[posIndex + 1].occupyingPiece == null) {
-                    tileRef[posIndex + 1].setAsViable(on);
-
-                    if (tileRef[posIndex + 2].occupyingPiece == null) {
-                        tileRef[posIndex + 2].setAsViable(on);
-
-                        if (tileRef[posIndex + 3].occupyingPiece == null) {
-                            tileRef[posIndex + 3].setAsViable(on);
-
-                            if (tileRef[posIndex + 4].occupyingPiece == null) {
-                                tileRef[posIndex + 4].setAsViable(on);
-
-                                if (tileRef[posIndex + 5].occupyingPiece == null) {
-                                    tileRef[posIndex + 5].setAsViable(on);
-
-                                    if (tileRef[posIndex + 6].occupyingPiece == null) {
-                                        tileRef[posIndex + 6].setAsViable(on);
-
-                                        if (tileRef[posIndex + 7].occupyingPiece == null) {
-                                            tileRef[posIndex + 7].setAsViable(on);
-                                        } else if (tileRef[posIndex + 7].occupyingPiece.team != team) {
-                                            tileRef[posIndex + 7].setAsViable(on);
-                                        }
-                                    } else if (tileRef[posIndex + 6].occupyingPiece.team != team) {
-                                        tileRef[posIndex + 6].setAsViable(on);
-                                    }
-                                } else if (tileRef[posIndex + 5].occupyingPiece.team != team) {
-                                    tileRef[posIndex + 5].setAsViable(on);
-                                }
-                            } else if (tileRef[posIndex + 4].occupyingPiece.team != team) {
-                                tileRef[posIndex + 4].setAsViable(on);
-                            }
-                        } else if (tileRef[posIndex + 3].occupyingPiece.team != team) {
-                            tileRef[posIndex + 3].setAsViable(on);
-                        }
-                    } else if (tileRef[posIndex + 2].occupyingPiece.team != team) {
-                        tileRef[posIndex + 2].setAsViable(on);
-                    }
-                } else if (tileRef[posIndex + 1].occupyingPiece.team != team) {
-                    tileRef[posIndex + 1].setAsViable(on);
-                }
-            }
-            case 1 -> {
-                tileRef[posIndex - 1].setAsViable(on);
-                tileRef[posIndex + 1].setAsViable(on);
-                tileRef[posIndex + 2].setAsViable(on);
-                tileRef[posIndex + 3].setAsViable(on);
-                tileRef[posIndex + 4].setAsViable(on);
-                tileRef[posIndex + 5].setAsViable(on);
-                tileRef[posIndex + 6].setAsViable(on);
-            }
-            case 2 -> {
-                tileRef[posIndex - 2].setAsViable(on);
-                tileRef[posIndex - 1].setAsViable(on);
-                tileRef[posIndex + 1].setAsViable(on);
-                tileRef[posIndex + 2].setAsViable(on);
-                tileRef[posIndex + 3].setAsViable(on);
-                tileRef[posIndex + 4].setAsViable(on);
-                tileRef[posIndex + 5].setAsViable(on);
-            }
-            case 3 -> {
-                tileRef[posIndex - 3].setAsViable(on);
-                tileRef[posIndex - 2].setAsViable(on);
-                tileRef[posIndex - 1].setAsViable(on);
-                tileRef[posIndex + 1].setAsViable(on);
-                tileRef[posIndex + 2].setAsViable(on);
-                tileRef[posIndex + 3].setAsViable(on);
-                tileRef[posIndex + 4].setAsViable(on);
-            }
-            case 4 -> {
-                tileRef[posIndex - 4].setAsViable(on);
-                tileRef[posIndex - 3].setAsViable(on);
-                tileRef[posIndex - 2].setAsViable(on);
-                tileRef[posIndex - 1].setAsViable(on);
-                tileRef[posIndex + 1].setAsViable(on);
-                tileRef[posIndex + 2].setAsViable(on);
-                tileRef[posIndex + 3].setAsViable(on);
-            }
-            case 5 -> {
-                tileRef[posIndex - 5].setAsViable(on);
-                tileRef[posIndex - 4].setAsViable(on);
-                tileRef[posIndex - 3].setAsViable(on);
-                tileRef[posIndex - 2].setAsViable(on);
-                tileRef[posIndex - 1].setAsViable(on);
-                tileRef[posIndex + 1].setAsViable(on);
-                tileRef[posIndex + 2].setAsViable(on);
-            }
-            case 6 -> {
-                tileRef[posIndex - 6].setAsViable(on);
-                tileRef[posIndex - 5].setAsViable(on);
-                tileRef[posIndex - 4].setAsViable(on);
-                tileRef[posIndex - 3].setAsViable(on);
-                tileRef[posIndex - 2].setAsViable(on);
-                tileRef[posIndex - 1].setAsViable(on);
-                tileRef[posIndex + 1].setAsViable(on);
-            }
-            case 7 -> {
-                tileRef[posIndex - 7].setAsViable(on);
-                tileRef[posIndex - 6].setAsViable(on);
-                tileRef[posIndex - 5].setAsViable(on);
-                tileRef[posIndex - 4].setAsViable(on);
-                tileRef[posIndex - 3].setAsViable(on);
-                tileRef[posIndex - 2].setAsViable(on);
-                tileRef[posIndex - 1].setAsViable(on);
+        for (int i = position.x; i < limiter; i++) {
+            if (tileRef[posIndex + i].occupyingPiece == null) {
+                tileRef[posIndex + i].setAsViable(on);
+            } else if (tileRef[posIndex + i].occupyingPiece != null && tileRef[posIndex + i].occupyingPiece.team != team) {
+                tileRef[posIndex + i].setAsViable(on);
+                break;
+            } else if (tileRef[posIndex + i].occupyingPiece != null && tileRef[posIndex + i].occupyingPiece.team == team) {
+                break;
             }
         }
     }
